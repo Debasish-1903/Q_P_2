45e8b339806a3105c77151360f9e0ac105ee91b7
Debasish Das <das31debasish@gmail.com>
2023-10-07 20:36:35 +0530

    metahacker cup practice .cpp

---
+++ stl/meta hacker cup.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define ull unsigned long long int
+#define mod 1000000007
+#define inf 1e9
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define f first
+#define s second
+#define nl "\n"
+#define pi pair<int,int>
+//#define v vector<long long int>
+#define vp vector<pair<int,int>>
+#define vvl vector<vector<long long int>>
+#define rep(n) for(int i=0 ; i<n ; i++)
+#define rei(i) for(int i=n ; i>0 ; i--)
+#define prio priority_queue
+#define np next_permutation
+#define N 200100
+
+#define yes "YES"
+#define no "NO"
+
+
+
+
+ /*ll add(ll a,ll b){
+	
+	ll ans=(a+b)%mod;
+	if(ans<0) ans+=mod;
+	
+	return ans;
+}
+
+ ll sub(ll a,ll b){
+	
+	ll ans=(a-b)%mod;
+	if(ans<0) ans+=mod;
+	
+	return ans;
+}
+
+ll mul(ll a,ll b){
+	
+	ll ans=(a*b)%mod;
+	if(ans<0) ans+=mod;
+	
+	return ans;
+}*/
+
+ll i=1;
+void solve(){
+
+ll s,d,k;
+
+cin>>s>>d>>k;
+
+
+if(s+(2*d)>=k&&(2*s+2*d)>=k+1){
+    
+    cout<<"Case #"<<i<<": "<<yes<<nl;
+    
+}else{
+    cout<<"Case #"<<i<<": "<<no<<nl;
+    
+    
+}
+
+i++;
+
+}
+
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	
+	 int _t; cin>>_t;while(_t--)
+		solve();
+	
+	
+	//cerr<<"Time elapsed: "<< 1.0*clock()/ CLOCKS_PER_SEC << "s.\n\n";
+	
+	
+	
+	
+}
