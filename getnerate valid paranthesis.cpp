602eac1700bd7c049912d955326ec21e1d7dc7c8
Debasish Das <das31debasish@gmail.com>
2023-10-26 00:04:37 +0530

    generate valid paranthesis easy version.cpp

---
+++ getnerate valid paranthesis.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define ull unsigned long long int
+#define mod 1000000007
+#define inf 1e9
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define f first
+#define s second
+#define nl "\n"
+#define pi pair<int,int>
+#define v vector<long long int>
+#define vp vector<pair<int,int>>
+#define vvl vector<vector<long long int>>
+#define rep(n) for(int i=0 ; i<n ; i++)
+#define rei(i) for(int i=n ; i>0 ; i--)
+#define prio priority_queue
+#define np next_permutation
+#define N 200100
+
+#define yes "YES"
+#define no "NO"
+
+
+
+
+ /*ll add(ll a,ll b){
+	
+	ll ans=(a+b)%mod;
+	if(ans<0) ans+=mod;
+	
+	return ans;
+}
+
+ ll sub(ll a,ll b){
+	
+	ll ans=(a-b)%mod;
+	if(ans<0) ans+=mod;
+	
+	return ans;
+}
+
+ll mul(ll a,ll b){
+	
+	ll ans=(a*b)%mod;
+	if(ans<0) ans+=mod;
+	
+	return ans;
+}*/
+
+void valid_paranthesis(ll open,ll close,string str, ll max){
+	
+	if(str.length()==max*2){
+		
+		cout<<str<<nl;
+		return;
+	}
+	
+	
+	if(open<max){
+		
+		valid_paranthesis(open+1,close,str+"(",max);
+	}
+	
+	if(close<open){
+		
+		valid_paranthesis(open,close+1,str+")",max);
+	}
+	
+	
+}
+
+
+
+void solve(){
+	
+	ll n;
+	cin>>n;
+	
+	valid_paranthesis(0,0,"",n/2);
+	
+	
+}
+
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	
+	// int _t; cin>>_t;while(_t--)
+		solve();
+	
+	
+	//cerr<<"Time elapsed: "<< 1.0*clock()/ CLOCKS_PER_SEC << "s.\n\n";
+	
+	
+	
+	
+}
