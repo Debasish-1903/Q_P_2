b015276c930062e4c0a60a89fc73ae42c4f4b578
Debasish Das <das31debasish@gmail.com>
2023-12-14 23:28:22 +0530

    14Maximum_Product_Subarray.cpp added

---
+++ dp/14Maximum_Product_Subarray.cpp
@@ -0,0 +1,94 @@
+Maximum Product Subarray
+
+Time-Limit: 1 sec Score: 0.00/100
+Difficulty : 
+ Memory: 256 MB Accepted Submissions: 100
+Relevant For:
+Description
+
+Given an integer array 
+n
+u
+m
+s
+, find a contiguous non-empty subarray within the array that has the largest product and return the product.
+
+The test cases are generated so that the answer will fit in a 
+64
+-bit integer.
+
+A subarray is a contiguous subsequence of the array.
+
+
+Input Format
+
+First-line contains 
+T
+ - the number of test cases.
+The first line of each test case contains 
+n
+ - the size of the array.
+The second line of each test case contains 
+n
+ elements of the array 
+n
+u
+m
+s
+.
+
+
+Output Format
+
+For each test case, print the maximum subarray product on a new line.
+
+
+Constraints
+
+1
+≤
+T
+≤
+100
+
+1
+≤
+n
+≤
+10
+5
+
+−
+10
+≤
+n
+u
+m
+s
+[
+i
+]
+≤
+10
+
+The test cases are generated so that the answer will fit in a 
+64
+-bit integer.
+
+
+Sample Input 1
+
+3
+4
+2 3 -2 4
+3
+-2 0 -1
+1
+-10
+
+Sample Output 1
+
+6
+0
+-10   */
+
